9ab31b7370e0e7a60398016958823acdc2c194a3
htariku <hgtariku@yahoo.com>
2021-12-05 01:41:42 -0500

    shop function

--- assets/game.js
+++ assets/game.js
@@ -16,38 +16,37 @@ console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + "now has " + enemyHealth + "health remaining."
 );
 
- // check enemy's health
  if (enemyHealth <= 0) {
     window.alert(enemyName + " has died!");
   } else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
 
-  // remove player's health by subtracting the amount set in the enemyAttack variable
+
   playerHealth = playerHealth - enemyAttck;
   console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
 
-  // check player's health
+
   if (playerHealth <= 0) {
     window.alert(playerName + " has died!");
   } else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
-  // if player choses to skip
+ 
 
     } else if (promptFight === "skip" || promptFight === "SKIP") {
-    // confirm player wants to skip
+
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
   
-    // if yes (true), leave fight
+  
     if (confirmSkip) {
       window.alert(playerName + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
+      
       playerMoney = playerMoney - 2;
     }
-    // if no (false), ask question again by running fight() again
+    
     else {
       fight();
     }
@@ -161,9 +160,13 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
       enemyHealth = 50;
       fight(pickedEnemyName);
+      if (i < enemyNames.length - 1) {
+        shop();
+      }
       if (playerHealth > 0 && i < enemyNames.length - 1) {
         var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
         if (storeConfirm) {
+          shop();
         }
       }
     }
@@ -190,3 +193,42 @@ var endGame = function() {
     window.alert('Thank you for playing Robot Gladiators! Come back soon!');
   }
 };
+var shop = function () {
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter: 'REFIL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+  switch (shopOptionPrompt) {
+    case "REFILL":
+    case "refill":
+      if (playerMoney >=7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+      case "UPGRADE":
+      case "upgrade":
+        if (playerMoney >= 7) {
+          window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+        break;
+        case "LEAVE":
+        case "leave":
+          window.alert("Leaving the store.");
+          break;
+          default:
+          window.alert("You did not pick a valid option. Try again.");
+          shop();
+          break;
+
+};
+}
